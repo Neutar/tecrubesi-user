ef84f6b2b3e247739902007a175eb7cf33b6aeca
Nur Ersoy Özyürek <nersoy88@gmail.com>
2020-05-26 21:09:24 +0000

    Merged in user-controller (pull request #2)
    [8] user controller
    
    Approved-by: Utkan Özyürek <utkanozyurek@gmail.com>

--- src/main/java/com/neutar/tecrubesi/user/controller/NeutarUserController.java
+++ src/main/java/com/neutar/tecrubesi/user/controller/NeutarUserController.java
@@ -1,19 +1,36 @@
 package com.neutar.tecrubesi.user.controller;
 
-import com.neutar.tecrubesi.user.dto.event.NeutarUserGainedBadgeEvent;
-import com.neutar.tecrubesi.user.listener.NeutarUserKafkaPublisher;
+import com.neutar.tecrubesi.user.dto.NeutarUserCreateDto;
+import com.neutar.tecrubesi.user.dto.NeutarUserUpdateDto;
+import com.neutar.tecrubesi.user.service.NeutarUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
+@Slf4j
 public class NeutarUserController {
-    private final NeutarUserKafkaPublisher neutarUserKafkaPublisher;
+    private final NeutarUserService neutarUserService;
 
-    @GetMapping("kafka")
-    private void getKafkaMessage(){
-        neutarUserKafkaPublisher.neutarUserGainedBadgeListener(NeutarUserGainedBadgeEvent.builder().build());
+    @PostMapping("/register")
+    public void registerUser(@NotNull @Valid @RequestBody NeutarUserCreateDto neutarUserCreateDto) {
+        log.info("registerUser called with neutarUserCreateDto={}", neutarUserCreateDto);
+        neutarUserService.registerUser(neutarUserCreateDto);
+        log.info("User created..");
+    }
+
+    @PutMapping("/update-user-detail/{userId}")
+    public void updateUserDetail(@PathVariable UUID userId, @NotNull @Valid @RequestBody NeutarUserUpdateDto
+            neutarUserUpdateDto) {
+        log.info("updateUserDetail called with userId={} neutarUserUpdateDto={}", userId, neutarUserUpdateDto);
+        neutarUserService.updateUserDetail(userId, neutarUserUpdateDto);
+        log.info("User detail updated..");
     }
 
 }
